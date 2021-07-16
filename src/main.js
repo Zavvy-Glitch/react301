b303a7b4a43e377c1ad3b81828fd364d8db6ca68
chattray <chea0073@gmail.com>
2021-07-15 20:53:51 -0700

    adding filter with forms

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Beasts from './beasts';
-// import data from './data.json';
-// import {Container, Row, Col} from 'react-bootstrap';
-// import Carousel from 'react-bootstrap/Carousel';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import './index.css';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+
 
 
 class Main extends React.Component {
@@ -32,29 +30,23 @@ handleImageClose = () => {
     test:false
   })
 }
+
 render() {
     return <>
-    {/* <Carousel fade> */}
-    <Card className="bg-dark text-white" style={{ width: '119rem' }}>
+    <div id = 'cardsArray'>
+    <Card className="bg-dark text-white" style={{ width: '90rem' }}>
     {this.props.data.map(currentBeast => {
       return (
         <Card.Body>
-       {/* <Carousel.Item> */}
       <Card.Text >
           <Beasts handleImageClick={this.handleImageClick} className="d-block w-100" beast={currentBeast}/>
       </Card.Text>
-       {/* </Carousel.Item> */}
     </Card.Body>
-        );
-        
+        ); 
       })}
-
     </Card>
-    {/* </Carousel> */}
+    </div>
   
-  
-  
-
     <Modal show = {this.state.test} onHide = {this.handleImageClose}>
        <Modal.Header>
          <Button onClick = {this.handleImageClose}>X</Button>
